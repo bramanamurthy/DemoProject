9e940b70e192fbd8eff799a8ceb54bcae6d38f8f
ramanamurthy <bure.ramanamurthy@gmail.com>
2019-07-07 20:55:19 +0530

    new files

---
+++ src/main/java/com/functions/Common.java
@@ -0,0 +1,14 @@
+package com.functions;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Common 
+{
+	public static void highlightElement(WebDriver driver,WebElement element) 
+	{
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].setAttribute('style','background:yellow;border:2px solid red;')", element);
+	}
+}
--- src/test/java/com/automationcicd/DemoProject/TestClass.java
+++ src/test/java/com/automationcicd/DemoProject/TestClass.java
@@ -1,9 +1,14 @@
 package com.automationcicd.DemoProject;
 
 import org.testng.annotations.Test;
+
+import com.functions.Common;
+
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,20 +16,40 @@ public class TestClass
 {
 	WebDriver driver;
 	
-	
 	@BeforeMethod
 	public void launchDriver()
 	{		
 		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
+		}
 	
 	@Test
 	public void test1()
 	{
-		driver.navigate().to("https://automationtalks.com/");
+		driver.get("https://www.makemytrip.com/flights/");
 		System.out.println("Test 1--> "+driver.getTitle());
+		
+		WebElement loginLink=driver.findElement(By.xpath("//p[text()=' Login or Create Account']"));
+		Common.highlightElement(driver, loginLink);
+		loginLink.click();
+		
+		System.out.println("Page Title: "+driver.getTitle());
+				
+		WebElement createAccount=driver.findElement(By.xpath("//a[text()=' Create New Account ']"));
+		Common.highlightElement(driver, createAccount);
+		createAccount.click();
+		
+		WebElement frameElm=driver.findElement(By.xpath("//iframe[@name='webpush-bubble']"));				
+		driver.switchTo().frame(frameElm);
+		
+		WebElement btnClose=driver.findElement(By.xpath("//button[@id='deny']"));
+		Common.highlightElement(driver, btnClose);
+		btnClose.click();
+				
+		driver.navigate().to("https://www.google.com/");
+		
+		System.out.println("Page Title: "+driver.getTitle());	
 		driver.quit();
 	}
 	
